3edb19162e2fd8d75ddc0b8319725575210b12b1
aarnavgautam <146917741+aarnavgautam@users.noreply.github.com>
2024-07-27 20:00:54 -0400

    Update decode.py

--- decode.py
+++ decode.py
@@ -1,5 +1,5 @@
 def decode(password):
-    original_password = ''
-    for i in password:
-        original_password += str((int(i) - 3) % 10)
-    return original_password
+    pw = ''
+    for char in password:
+        pw += str((int(char) - 3) % 10)
+    return pw
